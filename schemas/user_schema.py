eba3d03845c1a379262a3b0252aee9163840d9bf
Pham Duy Hai <phamduyhai01271105@gmail.com>
2026-06-22 16:09:28 +0700

    Tạo các pydantic model cho User và Token

---
+++ schemas/user_schema.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field, field_validator, ConfigDict, EmailStr
+
+
+class UserCreate(BaseModel):
+    model_config = ConfigDict(
+        str_strip_whitespace=False,
+        extra="forbid"
+    )
+    
+    email: EmailStr
+    username: str = Field(max_length=30)
+    password: str = Field(min_length=10, max_length=100, 
+                          description="Password phải có ít nhất 1 kí tự đặt biệt")
+    
+    @field_validator('username')
+    @classmethod
+    def validate_username(cls, value: str) -> str:
+        if not any(char.isalpha() for char in value):
+            raise ValueError("Username phải chứa ít nhất một chữ cái")
+        return value
+    
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        if all(char.isalnum() for char in value):
+            raise ValueError("Password phải chứ ít nhất một kí tự đặc biệt")
+        
+
+class UserLogin(BaseModel):
+    model_config = ConfigDict(
+        str_strip_whitespace=False, 
+        extra="forbid"
+    )
+    
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    username: str
+    is_active: bool
+    
